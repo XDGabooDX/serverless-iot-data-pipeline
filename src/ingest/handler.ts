3be120bbd10243e7afc2b0fd3c9e71fbde34e975
Gabriel Ahumada <gabrielfer30@gmail.com>
2026-01-22 14:13:09 -0600

    Add handler function for data ingestion

---
+++ src/ingest/handler.ts
@@ -0,0 +1,10 @@
+export const handler = async (event: any) => {
+  console.log("Received event:", JSON.stringify(event));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Data ingested successfully",
+    }),
+  };
+};
